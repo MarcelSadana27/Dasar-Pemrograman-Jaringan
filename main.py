adb748519c2898a12a59d52ab0a889a61c4bbb3e
MarcelSadana27 <47601949+MarcelSadana27@users.noreply.github.com>
2024-04-07 23:38:51 +0700

    too long var, i'll make it more simple

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
 from difflib import get_close_matches
 
-def load_knowledge_base(file_path: str) -> dict:
+def load_base(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
     return data
 
-def save_knowledge_base(file_path: str, data: dict):
+def save_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
@@ -19,16 +19,16 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
         if q["question"] == question:
             return q["answer"]
         
-def chat_bot():
-    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
+def chat():
+    knowledge_base: dict = load_base('knowledge_base.json')
 
     while True:
-        user_input: str = input('You: ')
+        userinput: str = input('You: ')
 
-        if user_input.lower() == 'quit':
+        if userinput.lower() == 'quit':
             break
 
-        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+        best_match: str | None = find_best_match(userinput, [q["question"] for q in knowledge_base["questions"]])
 
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
@@ -38,10 +38,10 @@ def chat_bot():
             new_answer: str = input('Your Answer ("skip" to cancel): ')
 
             if new_answer.lower() != 'skip':
-                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
-                save_knowledge_base('knowledge_base.json', knowledge_base)
+                knowledge_base["questions"].append({"question": userinput, "answer": new_answer})
+                save_base('knowledge_base.json', knowledge_base)
                 print('Bot: Thank you for your input.')
 
 
 if __name__ == '__main__':
-    chat_bot()          
+    chat()          
